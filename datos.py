93ad5a6a067986f008526ab86c380a48ab92ca24
MaxiCaceres97 <maxi.caceres.97@gmail.com>
2024-07-01 14:07:17 -0300

    borrado

--- datos.py
+++
@@ -1,4 +0,0 @@
-from jugador import Jugador
-
-jugador1 = Jugador("Jugador 1", "blancas")
-jugador2 = Jugador("Jugador 2", "negras")
--- jugador.py
+++
@@ -1,4 +0,0 @@
-class Jugador:
-    def __init__(self, nombre, color_piezas):
-        self.nombre = nombre
-        self.color_piezas = color_piezas
